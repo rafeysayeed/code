bb395995602ed032ba81a4d000a3d035b63877c9
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-06-14 06:40:46 +0500

    Time: 15 ms (90.11%), Space: 13.2 MB (97.49%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -4,9 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count = 0
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if (nums[i] == nums[j]):
-                    count += 1
-        return count
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if (nums[i] == nums[j]):
+        #             count += 1
+        # return count
+
+        dojo = {}
+        counter = 0
+        for i in nums:
+            friend_count = dojo.get(i,0)
+            counter += friend_count
+            dojo[i] = friend_count + 1
+        return counter
