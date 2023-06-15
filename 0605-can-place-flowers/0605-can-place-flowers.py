d23fc90c6a08c625720cd6301c32f2776b39960f
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-06-15 09:40:20 +0500

    Time: 128 ms (86.23%), Space: 14 MB (36.72%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -13,24 +13,31 @@ class Solution(object):
         [0,0]
         [0,0,1,0,0]
         """
-        for i in range(len(flowerbed)):
-            if (flowerbed[i] == 0):
-                if (i-1 < 0):
-                    if (i+1 == len(flowerbed)):
-                        flowerbed[i] = 1
-                        n -= 1
-                    elif (flowerbed[i+1] != 1):
-                        flowerbed[i] = 1
-                        n -= 1
-                elif (i == len(flowerbed)-1):
-                    if (flowerbed[i-1] != 1):
-                        flowerbed[i] = 1
-                        n -= 1
-                elif (flowerbed[i-1] != 1):
-                    if (flowerbed[i+1] != 1):
-                        flowerbed[i] = 1
-                        n -= 1
-            print(n)
-        if n <= 0: # can ask for planting less than available spots, n can be < 0
-            return True
-        return False
+        # for i in range(len(flowerbed)):
+        #     if (flowerbed[i] == 0):
+        #         if (i-1 < 0):
+        #             if (i+1 == len(flowerbed)):
+        #                 flowerbed[i] = 1
+        #                 n -= 1
+        #             elif (flowerbed[i+1] != 1):
+        #                 flowerbed[i] = 1
+        #                 n -= 1
+        #         elif (i == len(flowerbed)-1):
+        #             if (flowerbed[i-1] != 1):
+        #                 flowerbed[i] = 1
+        #                 n -= 1
+        #         elif (flowerbed[i-1] != 1):
+        #             if (flowerbed[i+1] != 1):
+        #                 flowerbed[i] = 1
+        #                 n -= 1
+        #     print(n)
+        # if n <= 0: # can ask for planting less than available spots, n can be < 0
+        #     return True
+        # return False
+
+        f = [0] + flowerbed + [0]
+        for i in range(1, len(f)-1):
+            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+                f[i] = 1
+                n -= 1
+        return n <= 0
