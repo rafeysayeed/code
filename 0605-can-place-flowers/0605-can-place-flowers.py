0755dc0413060fd266fafeb07f2d5cebdd442051
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-06-15 09:03:11 +0500

    Time: 299 ms (5.08%), Space: 14 MB (36.72%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -10,32 +10,10 @@ class Solution(object):
         [1,0,1,0,1,0,0,0,1]
         [1,0,1,0,0,1,0,0]
         [0]
-        [0.0]
+        [0,0]
+        [0,0,1,0,0]
         """
-        # for i in range(len(flowerbed)):
-        #     if flowerbed[i] == 1:
-        #         if i+2 < len(flowerbed) and flowerbed[i+2] == 0:
-        #             if i+2 == len(flowerbed) - 1:
-        #                     n -= 1
-        #                     flowerbed[i+2] = 1
-        #             elif i+3 < len(flowerbed) and flowerbed[i+3] == 0:
-        #                 n -= 1
-        #                 flowerbed[i+2] = 1
-        #     elif flowerbed[i] == 0:
-        #         if i == 0:
-        #             if i < len(flowerbed) - 1 and flowerbed[i+1] == 0:
-        #                 n -= 1
-        #                 flowerbed[i] = 1
-        #         elif i-1 > 0 and flowerbed[i-1] != 1:
-        #             if i < len(flowerbed) and flowerbed[i+1] == 0:
-        #                 n -= 1
-        #                 flowerbed[i] = 1
-        # if n == 0:
-        #     return True
-        # return False
-        # # [1,0,1,0,1,0,0]
         for i in range(len(flowerbed)):
-            #print(flowerbed)
             if (flowerbed[i] == 0):
                 if (i-1 < 0):
                     if (i+1 == len(flowerbed)):
@@ -46,13 +24,13 @@ class Solution(object):
                         n -= 1
                 elif (i == len(flowerbed)-1):
                     if (flowerbed[i-1] != 1):
-                        #print("chala")
                         flowerbed[i] = 1
                         n -= 1
                 elif (flowerbed[i-1] != 1):
                     if (flowerbed[i+1] != 1):
                         flowerbed[i] = 1
                         n -= 1
+            print(n)
         if n <= 0:
             return True
         return False
