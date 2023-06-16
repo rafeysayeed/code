4775aeb94b61b8ae6bb107f68b1a87440879bbd1
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-06-16 15:35:33 +0500

    Time: 11 ms (95.92%), Space: 13.3 MB (76.04%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,13 +5,26 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
+        
+        # # My Solution
+        # ret = ""
+        # ptr1 = ptr2 = 0
+        # while ptr1 < len(word1) or ptr2 < len(word2):
+        #     if ptr1 < len(word1):
+        #         ret += word1[ptr1]
+        #         ptr1 += 1
+        #     if ptr2 < len(word2):
+        #         ret += word2[ptr2]
+        #         ptr2 += 1
+        # return ret
+        
+        # Another Solution: Use single counter
         ret = ""
-        ptr1 = ptr2 = 0
-        while ptr1 < len(word1) or ptr2 < len(word2):
-            if ptr1 < len(word1):
-                ret += word1[ptr1]
-                ptr1 += 1
-            if ptr2 < len(word2):
-                ret += word2[ptr2]
-                ptr2 += 1
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                ret += word1[i]
+            if i < len(word2):
+                ret += word2[i]
+            i += 1
         return ret
