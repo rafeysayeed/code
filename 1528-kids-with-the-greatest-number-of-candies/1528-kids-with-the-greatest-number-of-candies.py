45e6f8550bb6930d5103a472f6c2a6b2f4b3f8d9
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-06-24 13:48:36 +0500

    Time: 22 ms (72.91%), Space: 13.3 MB (78.59%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -6,12 +6,14 @@ class Solution(object):
         :rtype: List[bool]
         """
         ret = []
+        maxima = max(candies)
         for i in range(len(candies)):
-            t = candies[i]
-            candies[i] += extraCandies
-            if candies[i] >= max(candies):
+            # t = candies[i]
+            # candies[i] += extraCandies # no need to use extra memory and steps
+            # if candies[i] >= max(candies): # no need to calculate at every iteration
+            if candies[i] + extraCandies >= maxima:
                 ret.append(True)
             else:
                 ret.append(False)
-            candies[i] = t
+            # candies[i] = t
         return ret
