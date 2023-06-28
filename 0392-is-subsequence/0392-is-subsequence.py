6ec5234a89ca1095003c9128718ca700dea57139
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-06-28 13:08:51 +0500

    Time: 19 ms (59.33%), Space: 13.8 MB (10.6%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,15 +5,24 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        ls, lt = len(s), len(t)
+        # ls, lt = len(s), len(t)
+        # ptr = 0
+        # if ls and not lt:
+        #         return False
+        # elif not ls:
+        #         return True
+        # for i in range(lt):
+        #     if t[i] == s[ptr]:
+        #         ptr += 1
+        #     if ptr == ls:
+        #         return True
+        # return False
+        
         ptr = 0
-        if ls and not lt:
-                return False
-        elif not ls:
-                return True
+        ls, lt = len(s), len(t)
         for i in range(lt):
+            if ls == ptr:
+                break
             if t[i] == s[ptr]:
                 ptr += 1
-            if ptr == ls:
-                return True
-        return False
+        return ptr == ls
