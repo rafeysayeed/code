19ed3262a71f0b75e1b18041cadeb5e4abba2f8f
rafeysayeed <89657249+rafeysayeed@users.noreply.github.com>
2023-07-04 22:47:13 +0500

    Time: 1020 ms (89.95%), Space: 22.6 MB (98.55%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -5,20 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-        # Sliding window will not work when k = 32483 (Time Limit Exceeds)
-        # maxima = None
-        # for i in range(len(nums)):
-        #     if i + k - 1 < len(nums):
-        #         val = sum(nums[i:i+k])
-        #         if maxima == None or val > maxima:
-        #             maxima = float(val)
-        #     else:
-        #         break
-        # return maxima / k
-
-        mid = sum(nums[0:k])
-        maxima = float(mid)
+        maximus = float(sum(nums[:k]))
+        summa = maximus
         for i in range(len(nums)-k):
-            mid += nums[i+k] - nums[i]
-            maxima = float(max(maxima, mid))
-        return maxima/k
+            summa = summa - nums[i] + nums[k+i] #sum(nums[i+1:k+i+1])
+            if summa > maximus:
+                maximus = float(summa)
+        return float(maximus / k)
