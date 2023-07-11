7104e6834cd95fe2db4a7f016edfdb46682e4eae
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-07-11 09:12:05 +0500

    Time: 49 ms (43.89%), Space: 14.6 MB (44.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,12 +5,25 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        hashmap1, hashmap2 = {}, {}
-        for i in range(len(s)):
-            hashmap1[s[i]] = hashmap1.get(s[i], 0) + 1
-            hashmap2[t[i]] = hashmap2.get(t[i], 0) + 1
-        if hashmap1 != hashmap2:
-            return False
-        return True
+        # if len(s) != len(t):
+        #     return False
+        # hashmap1, hashmap2 = {}, {}
+        # for i in range(len(s)):
+        #     hashmap1[s[i]] = hashmap1.get(s[i], 0) + 1
+        #     hashmap2[t[i]] = hashmap2.get(t[i], 0) + 1
+        # if hashmap1 != hashmap2:
+        #     return False
+        # return True
+
+        x, y = sorted(s), sorted(t)
+        return x == y
+
+        # hashmap = defaultdict(int)
+        # for i in s:
+        #     hashmap[i] += 1
+        # for i in t:
+        #     hashmap[i] -= 1
+        # for v in hashmap.values():
+        #     if v != 0:
+        #         return False
+        # return True
