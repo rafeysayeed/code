62fb8691c5e4095adcc63b4742d5b84a4205c237
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-07-11 09:16:41 +0500

    Time: 510 ms (24.01%), Space: 29.3 MB (6.22%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,7 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        num_set = set(nums)
-        if len(nums) != len(num_set):
-            return True
+        # num_set = set(nums)
+        # if len(nums) != len(num_set):
+        #     return True
+        # return False
+
+        # Another approach
+        num_set = set()
+        for i in nums:
+            if i in num_set:
+                return True
+            num_set.add(i)
         return False
