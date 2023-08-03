586a92ba80ab42a98bc38dd83e03289ed6291d8e
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-08-03 12:16:52 +0500

    Time: 2536 ms (6.1%), Space: 14.5 MB (58.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -9,16 +9,16 @@ class Solution(object):
         [0,1,0,3,12]
         [1,1,0]
         """
-        # My solution
-        point = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                if point != i:
-                    nums[point] = nums[i]
-                    nums[i] = 0
-                    point += 1
-                else:
-                    point += 1
+        # # My solution
+        # point = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         if point != i:
+        #             nums[point] = nums[i]
+        #             nums[i] = 0
+        #             point += 1
+        #         else:
+        #             point += 1
         # # Snowball
         # snowballSize = 0
         # for i in range(len(nums)):
@@ -27,3 +27,18 @@ class Solution(object):
         #     elif snowballSize > 0:
         #         nums[i - snowballSize] = nums[i]
         #         nums[i] = 0
+        left = 0
+        right = 0
+        while right < len(nums):
+            while left < len(nums) and nums[left] != 0:
+                left += 1
+            right = left
+            while right < len(nums) and nums[right] == 0:
+                right += 1
+            if right >= len(nums):
+                break
+            temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+
+        return nums
