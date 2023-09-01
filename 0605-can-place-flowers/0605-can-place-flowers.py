5f41de02de96015a7f8211e4a0ca65cc730425dd
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-09-01 15:45:55 +0500

    Time: 116 ms (80.73%), Space: 14 MB (49.8%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -4,40 +4,14 @@ class Solution(object):
         :type flowerbed: List[int]
         :type n: int
         :rtype: bool
-        [1,0,0,0,1]
-        [0,1,0,1,0]
-        [0,0,1]
-        [1,0,1,0,1,0,0,0,1]
-        [1,0,1,0,0,1,0,0]
-        [0]
-        [0,0]
-        [0,0,1,0,0]
         """
-        # for i in range(len(flowerbed)):
-        #     if (flowerbed[i] == 0):
-        #         if (i-1 < 0):
-        #             if (i+1 == len(flowerbed)):
-        #                 flowerbed[i] = 1
-        #                 n -= 1
-        #             elif (flowerbed[i+1] != 1):
-        #                 flowerbed[i] = 1
-        #                 n -= 1
-        #         elif (i == len(flowerbed)-1):
-        #             if (flowerbed[i-1] != 1):
-        #                 flowerbed[i] = 1
-        #                 n -= 1
-        #         elif (flowerbed[i-1] != 1):
-        #             if (flowerbed[i+1] != 1):
-        #                 flowerbed[i] = 1
-        #                 n -= 1
-        #     print(n)
-        # if n <= 0: # can ask for planting less than available spots, n can be < 0
-        #     return True
-        # return False
-
-        f = [0] + flowerbed + [0]
-        for i in range(1, len(f)-1):
-            if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
-                f[i] = 1
-                n -= 1
-        return n <= 0
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                if i > 0 and flowerbed[i-1] == 0 and i < len(flowerbed)-1 and flowerbed[i+1] == 0:
+                    flowerbed[i] = 1
+                    n -= 1
+        if n > 0:
+            return False
+        else:
+            return True
