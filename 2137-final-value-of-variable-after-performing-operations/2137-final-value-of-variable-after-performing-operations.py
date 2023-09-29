4e3cb093590551331956bf735c569c9ab07a5d16
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-09-29 08:43:48 +0500

    Time: 31 ms (39.03%), Space: 13.4 MB (13.7%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -5,19 +5,19 @@ class Solution(object):
         :rtype: int
         """
         # Copied 2
-        x = 0
-        for i in operations:
-            x += 44 - ord(i[1])
-        return x
-
-        # Copied 1
         # x = 0
         # for i in operations:
-        #     if i[1] == "-":
-        #         x -= 1
-        #     else:
-        #         x += 1
+        #     x += 44 - ord(i[1])
         # return x
+
+        # Copied 1
+        x = 0
+        for i in operations:
+            if i[1] == "-":
+                x -= 1
+            else:
+                x += 1
+        return x
 
         # My solution
         # x = 0
