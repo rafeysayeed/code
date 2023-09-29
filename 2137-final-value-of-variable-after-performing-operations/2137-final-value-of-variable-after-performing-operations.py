02cbd316c5007c81d98d75962a44827fb31fa5d5
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-09-29 08:37:19 +0500

    Time: 31 ms (39.03%), Space: 13.3 MB (41.09%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        x = 0
+        for i in operations:
+            if i[0] == "X":
+                if i[1:] == "--":
+                    x -= 1
+                else:
+                    x += 1
+            else:
+                if i[0:2] == "--":
+                    x -= 1
+                else:
+                    x += 1
+        return x
