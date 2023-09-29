42ac47e45fbd45f9d2ffc99de33f968468c5a432
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-09-29 08:39:01 +0500

    Time: 27 ms (70.4%), Space: 13.2 MB (93.3%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.py
@@ -4,16 +4,26 @@ class Solution(object):
         :type operations: List[str]
         :rtype: int
         """
+        # Copied
         x = 0
         for i in operations:
-            if i[0] == "X":
-                if i[1:] == "--":
-                    x -= 1
-                else:
-                    x += 1
+            if i[1] == "-":
+                x -= 1
             else:
-                if i[0:2] == "--":
-                    x -= 1
-                else:
-                    x += 1
+                x += 1
         return x
+
+        # My solution
+        # x = 0
+        # for i in operations:
+        #     if i[0] == "X":
+        #         if i[1:] == "--":
+        #             x -= 1
+        #         else:
+        #             x += 1
+        #     else:
+        #         if i[0:2] == "--":
+        #             x -= 1
+        #         else:
+        #             x += 1
+        # return x
