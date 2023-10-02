9c0f21d1f3205187dd7c6f0dba7a5a7468629233
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-02 15:22:22 +0500

    Time: 15 ms (51.76%), Space: 13.3 MB (71.64%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -5,16 +5,11 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        l1 = len(word1)
-        l2 = len(word2)
-        c1 = 0
-        c2 = 0
-        merged = ""
-        while c1 != l1 or c2 != l2:
-            if c1 < l1:
-                merged += word1[c1]
-                c1 += 1
-            if c2 < l2:
-                merged += word2[c2]
-                c2 += 1
-        return merged
+        newStr = ""
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                newStr += word1[i]
+            if i < len(word2):
+                newStr += word2[i]
+        return newStr    
+        
