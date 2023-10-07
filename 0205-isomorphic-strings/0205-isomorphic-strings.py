98892878e8569719557191a8d57fb57f064dee92
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-07 11:45:06 +0500

    Time: 29 ms (43.87%), Space: 16.5 MB (5.38%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -5,27 +5,38 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-
-        hashMap1 = {}
-        for i in range(len(s)):
-            if s[i] not in hashMap1:
-                hashMap1[s[i]] = t[i]
-            else:
-                if t[i] == hashMap1[s[i]]:
-                    continue
-                else:
-                    return False
-        hashMap2 = {}
-        for i in range(len(t)):
-            if t[i] not in hashMap2:
-                hashMap2[t[i]] = s[i]
-            else:
-                if s[i] == hashMap2[t[i]]:
-                    continue
-                else:
-                    return False
+        # NeetCode
+        hash1, hash2 = {}, {}
+        for c1, c2 in zip(s, t):
+            if ((c1 in hash1 and hash1[c1] != c2) or
+            (c2 in hash2 and hash2[c2] != c1)):
+                return False
+            hash1[c1] = c2
+            hash2[c2] = c1
         return True
 
+        # Intuition 2
+        # hashMap1 = {}
+        # for i in range(len(s)):
+        #     if s[i] not in hashMap1:
+        #         hashMap1[s[i]] = t[i]
+        #     else:
+        #         if t[i] == hashMap1[s[i]]:
+        #             continue
+        #         else:
+        #             return False
+        # hashMap2 = {}
+        # for i in range(len(t)):
+        #     if t[i] not in hashMap2:
+        #         hashMap2[t[i]] = s[i]
+        #     else:
+        #         if s[i] == hashMap2[t[i]]:
+        #             continue
+        #         else:
+        #             return False
+        # return True
+
+        # Intuition 1
         # if len(s) != len(t):
         #     return False
 
