4ea3df787887fc545bf015acc2a490890ba4b9ef
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-07 11:35:57 +0500

    Time: 18 ms (90.67%), Space: 14.6 MB (51.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -5,23 +5,44 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
 
-        hashMap1, hashMap2 = {}, {}
+        hashMap1 = {}
         for i in range(len(s)):
             if s[i] not in hashMap1:
-                hashMap1[s[i]] = [i]
+                hashMap1[s[i]] = t[i]
             else:
-                hashMap1[s[i]] += [i]
+                if t[i] == hashMap1[s[i]]:
+                    continue
+                else:
+                    return False
+        hashMap2 = {}
+        for i in range(len(t)):
             if t[i] not in hashMap2:
-                hashMap2[t[i]] = [i]
+                hashMap2[t[i]] = s[i]
             else:
-                hashMap2[t[i]] += [i]
-
-        for k, v in hashMap1.items():
-            if v in hashMap2.values():
-                continue
-            else:
-                return False
+                if s[i] == hashMap2[t[i]]:
+                    continue
+                else:
+                    return False
         return True
+
+        # if len(s) != len(t):
+        #     return False
+
+        # hashMap1, hashMap2 = {}, {}
+        # for i in range(len(s)):
+        #     if s[i] not in hashMap1:
+        #         hashMap1[s[i]] = [i]
+        #     else:
+        #         hashMap1[s[i]] += [i]
+        #     if t[i] not in hashMap2:
+        #         hashMap2[t[i]] = [i]
+        #     else:
+        #         hashMap2[t[i]] += [i]
+
+        # for k, v in hashMap1.items():
+        #     if v in hashMap2.values():
+        #         continue
+        #     else:
+        #         return False
+        # return True
