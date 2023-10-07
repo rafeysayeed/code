45660cb14e3b80c03a530d6faea60f73c9512fd5
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-07 10:47:40 +0500

    Time: 53 ms (40.93%), Space: 13.4 MB (84.96%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        hashMap1, hashMap2 = {}, {}
+        for i in magazine:
+            if i not in hashMap1:
+                hashMap1[i] = 1
+            else:
+                hashMap1[i] += 1
+        for i in ransomNote:
+            if i not in hashMap2:
+                hashMap2[i] = 1
+            else:
+                hashMap2[i] += 1
+        for k, v in hashMap2.items():
+            if k in hashMap1 and v <= hashMap1[k]:
+                continue
+            else:
+                return False
+        return True
