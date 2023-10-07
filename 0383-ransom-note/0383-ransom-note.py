7275af37d3c4ee4f5402c73655c6ce4326ac2714
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-07 10:51:35 +0500

    Time: 49 ms (47.08%), Space: 13.5 MB (32.38%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -5,20 +5,33 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        hashMap1, hashMap2 = {}, {}
+        hashMap = {}
         for i in magazine:
-            if i not in hashMap1:
-                hashMap1[i] = 1
+            if i not in hashMap:
+                hashMap[i] = 1
             else:
-                hashMap1[i] += 1
+                hashMap[i] += 1
         for i in ransomNote:
-            if i not in hashMap2:
-                hashMap2[i] = 1
-            else:
-                hashMap2[i] += 1
-        for k, v in hashMap2.items():
-            if k in hashMap1 and v <= hashMap1[k]:
-                continue
+            if i in hashMap and hashMap[i] > 0:
+                hashMap[i] -= 1
             else:
                 return False
         return True
+
+        # hashMap1, hashMap2 = {}, {}
+        # for i in magazine:
+        #     if i not in hashMap1:
+        #         hashMap1[i] = 1
+        #     else:
+        #         hashMap1[i] += 1
+        # for i in ransomNote:
+        #     if i not in hashMap2:
+        #         hashMap2[i] = 1
+        #     else:
+        #         hashMap2[i] += 1
+        # for k, v in hashMap2.items():
+        #     if k in hashMap1 and v <= hashMap1[k]:
+        #         continue
+        #     else:
+        #         return False
+        # return True
