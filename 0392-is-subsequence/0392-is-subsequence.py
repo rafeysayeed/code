1dfa2f6b4fa42af8c40a8fb664d44b70f8da941c
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-13 12:41:36 +0500

    Time: 15 ms (57.38%), Space: 13.1 MB (98.32%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,24 +5,12 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # ls, lt = len(s), len(t)
-        # ptr = 0
-        # if ls and not lt:
-        #         return False
-        # elif not ls:
-        #         return True
-        # for i in range(lt):
-        #     if t[i] == s[ptr]:
-        #         ptr += 1
-        #     if ptr == ls:
-        #         return True
-        # return False
-        
-        ptr = 0
-        ls, lt = len(s), len(t)
-        for i in range(lt):
-            if ls == ptr:
-                break
-            if t[i] == s[ptr]:
-                ptr += 1
-        return ptr == ls
+        A = 0
+        B = 0
+        while A < len(s) and B < len(t):
+            if s[A] == t[B]:
+                A += 1
+                B += 1
+            else:
+                B += 1
+        return A == len(s)
