02d665a45153d52b55291b735df286c4401bba13
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-15 18:07:10 +0500

    Time: 37 ms (48.88%), Space: 14.6 MB (32.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,16 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        hash1, hash2 = {}, {}
-        for i in range(len(s)):
-            if s[i] not in hash1:
-                hash1[s[i]] = 1
-            else:
-                hash1[s[i]] += 1
-            if t[i] not in hash2:
-                hash2[t[i]] = 1
-            else:
-                hash2[t[i]] += 1
-        return hash1 == hash2
+        s, t = sorted(s), sorted(t)
+        return s == t
+        # if len(s) != len(t):
+        #     return False
+        # hash1, hash2 = {}, {}
+        # for i in range(len(s)):
+        #     if s[i] not in hash1:
+        #         hash1[s[i]] = 1
+        #     else:
+        #         hash1[s[i]] += 1
+        #     if t[i] not in hash2:
+        #         hash2[t[i]] = 1
+        #     else:
+        #         hash2[t[i]] += 1
+        # return hash1 == hash2
