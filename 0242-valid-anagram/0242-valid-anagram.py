cd888068f3aae7d21a101ddec509b87b42ac4bf8
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-15 18:06:41 +0500

    Time: 20 ms (96.57%), Space: 15.2 MB (17.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,35 +5,16 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # if len(s) != len(t):
-        #     return False
-        # hashmap1, hashmap2 = {}, {}
-        # for i in range(len(s)):
-        #     hashmap1[s[i]] = hashmap1.get(s[i], 0) + 1
-        #     hashmap2[t[i]] = hashmap2.get(t[i], 0) + 1
-        # if hashmap1 != hashmap2:
-        #     return False
-        # return True
-
-        # x, y = sorted(s), sorted(t)
-        # return x == y
-
-        # hashmap = defaultdict(int)
-        # for i in s:
-        #     hashmap[i] += 1
-        # for i in t:
-        #     hashmap[i] -= 1
-        # for v in hashmap.values():
-        #     if v != 0:
-        #         return False
-        # return True
-
         if len(s) != len(t):
             return False
-        memo = {}
-        for i in s:
-            if i not in memo:
-                if s.count(i) != t.count(i):
-                    return False
-                memo[i] = None
-        return True
+        hash1, hash2 = {}, {}
+        for i in range(len(s)):
+            if s[i] not in hash1:
+                hash1[s[i]] = 1
+            else:
+                hash1[s[i]] += 1
+            if t[i] not in hash2:
+                hash2[t[i]] = 1
+            else:
+                hash2[t[i]] += 1
+        return hash1 == hash2
