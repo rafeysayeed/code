676b4b40217157a7f1ff2661d5652baeb6bf05d5
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-10-18 09:37:12 +0500

    Time: 44 ms (99.13%), Space: 15 MB (20.39%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,10 +5,19 @@ class Solution(object):
         :rtype: int
         [1,1,1] -> [1]
         """
-        seen = []
-        for i in nums:
-            if i not in seen:
-                seen += [i]
-        for i in range(len(seen)):
-            nums[i] = seen[i]
-        return len(seen)
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[i] != nums[j]: # ← indicates unique element
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+
+
+        # # My Sol
+        # seen = []
+        # for i in nums:
+        #     if i not in seen:
+        #         seen += [i]
+        # for i in range(len(seen)):
+        #     nums[i] = seen[i]
+        # return len(seen)
