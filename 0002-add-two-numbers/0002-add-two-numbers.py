18b6fa2f3e7b9b8a4df1581f69f36fc8142e6e53
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-11-01 09:35:22 +0500

    Time: 53 ms (17.16%), Space: 13.3 MB (68.68%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -34,35 +34,71 @@ class Solution(object):
             addition of new index
         """
 
-        # approach 1: 
-        digit1, digit2 = [], []
+        # approach 1: 18m
+        # digit1, digit2 = [], []
+        # temp1, temp2 = l1, l2
+        # while temp1 != None:
+        #     digit1.append(temp1.val)
+        #     temp1 = temp1.next
+        # while temp2 != None:
+        #     digit2.append(temp2.val)
+        #     temp2 = temp2.next
+        # number1, number2 = 0, 0
+        # tenPo = 1
+        # for i in digit1:
+        #     number1 += i * tenPo
+        #     tenPo *= 10
+        # tenPo = 1
+        # for i in digit2:
+        #     number2 += i * tenPo
+        #     tenPo *= 10
+        # result = number1 + number2
+        # tenPo = 10
+        # resultNode = None
+        # curr = None
+        # while result > 0:
+        #     catch = result % tenPo
+        #     result //= tenPo
+        #     if not resultNode:
+        #         resultNode = ListNode(catch)
+        #         curr = resultNode
+        #     else:
+        #         curr.next = ListNode(catch)
+        #         curr = curr.next
+        # return resultNode if resultNode else ListNode(0)
+
+        # approach 2:
         temp1, temp2 = l1, l2
-        while temp1 != None:
-            digit1.append(temp1.val)
-            temp1 = temp1.next
-        while temp2 != None:
-            digit2.append(temp2.val)
-            temp2 = temp2.next
-        number1, number2 = 0, 0
+        carry = 0
         tenPo = 1
-        for i in digit1:
-            number1 += i * tenPo
-            tenPo *= 10
-        tenPo = 1
-        for i in digit2:
-            number2 += i * tenPo
-            tenPo *= 10
-        result = number1 + number2
-        tenPo = 10
-        resultNode = None
-        curr = None
-        while result > 0:
-            catch = result % tenPo
-            result //= tenPo
+        resultNode, curr = None, None
+        while temp1 or temp2 or carry:
+            if temp1 and temp2:
+                A = temp1.val
+                B = temp2.val
+                temp1 = temp1.next
+                temp2 = temp2.next
+            elif temp1:
+                A = temp1.val
+                B = 0
+                temp1 = temp1.next
+            elif temp2:
+                A = 0
+                B = temp2.val
+                temp2 = temp2.next
+            else:
+                A = 0
+                B = 0
+            C = A + B + carry
+            if C > 9:
+                carry = 1
+                C %= 10
+            else:
+                carry = 0
             if not resultNode:
-                resultNode = ListNode(catch)
+                resultNode = ListNode(C)
                 curr = resultNode
             else:
-                curr.next = ListNode(catch)
+                curr.next = ListNode(C)
                 curr = curr.next
-        return resultNode if resultNode else ListNode(0)
+        return resultNode 
