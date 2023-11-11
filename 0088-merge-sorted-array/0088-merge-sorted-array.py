43ac385eda5f75fa6a6b2ee213cf394013121601
Rafey Sayeed <57788288+rafeysayeed@users.noreply.github.com>
2023-11-11 12:04:37 +0500

    Time: 18 ms (67.45%), Space: 13.4 MB (12.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,22 +7,16 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-
-        # two pointers
         i = m-1
         j = n-1
         k = m+n-1
-        while(j >= 0):
-            if (i >= 0 and nums1[i] > nums2[j]):
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
-                k -= 1
                 i -= 1
+                k -= 1
             else:
                 nums1[k] = nums2[j]
-                k -= 1
                 j -= 1
-
-        # using in-built sort
-        # for i in range(n):
-        #     nums1[m+i] = nums2[i]
-        # return nums1.sort()
+                k -= 1
+        
