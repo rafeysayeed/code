685a7a3d13303d0c5f42480c23374aed111ccbfc
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-04-08 10:13:36 +0500

    Time: 18 ms (5.08%), Space: 19.7 MB (92.72%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.php
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.php
@@ -9,24 +9,24 @@ class Solution {
         $rs="";
         $l1=strlen($word1);
         $l2=strlen($word2);
-        $min=min($l1,$l2);
+        $min=max($l1,$l2);
         for($i=0;$i<$min;$i++){
             if($i<$l1)
                 $rs.=$word1[$i];
             if($i<$l2)
                 $rs.=$word2[$i];
         }
-        $diff=$l1-$l2;
-        if($diff<0){
-            for($i=$min;$i<$l2;$i++){
-                $rs.=$word2[$i];
-            }
-        }
-        else{
-            for($i=$min;$i<$l1;$i++){
-                $rs.=$word1[$i];
-            }
-        }
+        // $diff=$l1-$l2;
+        // if($diff<0){
+        //     for($i=$min;$i<$l2;$i++){
+        //         $rs.=$word2[$i];
+        //     }
+        // }
+        // else{
+        //     for($i=$min;$i<$l1;$i++){
+        //         $rs.=$word1[$i];
+        //     }
+        // }
         return $rs;
     }
 }
