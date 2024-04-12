e15183bac80b9b9ad971db36b709dca6347e387e
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-04-12 08:14:38 +0500

    Time: 126 ms (11.22%), Space: 13.5 MB (13.05%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -5,12 +5,25 @@ class Solution(object):
         :rtype: int
         [1,1,1] -> [1]
         """
-        i = 0
-        for j in range(1, len(nums)):
-            if nums[i] != nums[j]: # ← indicates unique element
+        arr = []
+        i = 1
+        while (i < len(nums)):
+            if nums[i] == nums[i-1]:
+                arr += [nums.pop(i)]
+            else:
                 i += 1
-                nums[i] = nums[j]
-        return i + 1
+        k = len(nums)
+        for i in range(len(arr)):
+            nums += [arr[i]]
+        return k
+            
+
+        # i = 0
+        # for j in range(1, len(nums)):
+        #     if nums[i] != nums[j]: # ← indicates unique element
+        #         i += 1
+        #         nums[i] = nums[j]
+        # return i + 1
 
 
         # # My Sol
@@ -21,3 +34,16 @@ class Solution(object):
         # for i in range(len(seen)):
         #     nums[i] = seen[i]
         # return len(seen)
+
+        # i = 1
+        # while i < len(nums):
+        #     if nums[i-1] == nums[i]:
+        #         to_replace = (i, nums[i])
+        #         while i < len(nums) and nums[i] == to_replace[1]:
+        #             i += 1
+        #         if i < len(nums):
+        #             nums[to_replace[0]] = nums[i]
+        #             i = to_replace[0] + 1
+        #     else:
+        #         i += 1
+        # return to_replace[0] + 1
