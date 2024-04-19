6326d10567a72578e6e648f06481bff68b9b8da3
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-04-19 11:18:02 +0500

    Time: 19 ms (24.17%), Space: 11.8 MB (67.69%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,12 +5,9 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        A = 0
-        B = 0
-        while A < len(s) and B < len(t):
-            if s[A] == t[B]:
-                A += 1
-                B += 1
-            else:
-                B += 1
-        return A == len(s)
+        i, j = 0, 0
+        while i < len(t):
+            if j < len(s) and t[i] == s[j]:
+                j += 1
+            i += 1
+        return j == len(s)
