643070836edeef0408f591a8a1f59aa3350ffac3
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-05-02 12:22:23 +0500

    Time: 712 ms (9.13%), Space: 16.2 MB (38.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -6,37 +6,48 @@ class Solution(object):
         Q. What if there are duplicates
         Q. What if the element being checked is already in another list (an anagram with two different words) -> In this case, both of them will be in the same list
         """
-
-        if not len(strs):
-            return [[""]]
-        
-        def MapIt(s):
-            hashMap = {}
-            for i in s:
-                if i not in hashMap:
-                    hashMap[i] = 1
-                else:
-                    hashMap[i] += 1
-            return hashMap
-
-        listOfHash = []
-        returnList = []
+        hashM = {}
         for i in range(len(strs)):
-            hashA = MapIt(strs[i])
-            if hashA not in listOfHash:
-                listOfHash.append(hashA)
-                returnList.append([strs[i]])
+            ele = str(strs[i])
+            sele = ele
+            sele = ''.join(sorted(sele))
+            print(ele, sele)
+            if sele in hashM.keys():
+                hashM[sele] += [ele]
             else:
-                for j in range(len(listOfHash)):
-                    if hashA == listOfHash[j]:
-                        returnList[j].append(strs[i])
-        return returnList
+                hashM[sele] = [ele]
+        print(hashM)
+        retL = []
+        for v in hashM.values():
+            retL.append(v)
+        return retL
 
 
-        # for d in list_of_dicts:
-        #     if d == your_dict:
-        #         print("Found a matching dictionary!")
-        #         break  # If you want to stop searching after finding the first match
+
+        # if not len(strs):
+        #     return [[""]]
+        
+        # def MapIt(s):
+        #     hashMap = {}
+        #     for i in s:
+        #         if i not in hashMap:
+        #             hashMap[i] = 1
+        #         else:
+        #             hashMap[i] += 1
+        #     return hashMap
+
+        # listOfHash = []
+        # returnList = []
+        # for i in range(len(strs)):
+        #     hashA = MapIt(strs[i])
+        #     if hashA not in listOfHash:
+        #         listOfHash.append(hashA)
+        #         returnList.append([strs[i]])
+        #     else:
+        #         for j in range(len(listOfHash)):
+        #             if hashA == listOfHash[j]:
+        #                 returnList[j].append(strs[i])
+        # return returnList
 
         # N^2 Time Limit Exceeded
         # returnList = []
