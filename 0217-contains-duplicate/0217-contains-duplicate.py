8ba957f900536fa02294137299dba17d202a8132
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-06-02 14:11:31 +0500

    Time: 665 ms (5.11%), Space: 21.3 MB (97.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums = sorted(nums)
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
+        nums.sort()
+        print (nums)
+        for i in range(1, len(nums)):
+            if nums[i-1] == nums[i]:
                 return True
         return False
