7e9bca8963cdd8126efdc73a615d92c91d0f1c31
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-06-20 12:45:26 +0500

    Time: 284 ms (60.5%), Space: 19.1 MB (72.15%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -9,6 +9,7 @@ class Solution(object):
         [abcd bcd cd d]
         [1 a ab abc] - pre
         [bcd cd d 1]
+        [bcd acd abd abc]
         """
         # bruteforce O(n^2)
         # answer = []
@@ -22,15 +23,26 @@ class Solution(object):
         # return answer
 
         # making prefix
-        pre = [1] * len(nums)
-        for i in range(1, len(nums)):
-            pre[i] = pre[i-1] * nums[i-1]
-        # making postfix
-        post = [1] * len(nums)
-        for i in range(len(nums)-2, -1, -1):
-            post[i] = post[i+1] * nums[i+1]
-        # combining
-        answer = []
+        # pre = [1] * len(nums)
+        # for i in range(1, len(nums)):
+        #     pre[i] = pre[i-1] * nums[i-1]
+        # # making postfix
+        # post = [1] * len(nums)
+        # for i in range(len(nums)-2, -1, -1):
+        #     post[i] = post[i+1] * nums[i+1]
+        # # combining
+        # answer = []
+        # for i in range(len(nums)):
+        #     answer += [pre[i]*post[i]]
+        # return answer
+
+        answer = [1] * len(nums)
+        curr = 1
         for i in range(len(nums)):
-            answer += [pre[i]*post[i]]
+            answer[i] *= curr
+            curr *= nums[i]
+        curr = 1
+        for i in range(len(nums)-1, -1, -1):
+            answer[i] *= curr
+            curr *= nums[i]
         return answer
