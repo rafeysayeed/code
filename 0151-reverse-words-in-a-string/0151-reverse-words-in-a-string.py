9f6e4bb4eeed18537a9c61db2d9d57fea850f3fb
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-07-25 00:43:59 +0500

    Time: 34 ms (5.32%), Space: 12 MB (19.77%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,18 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        newStr = ""
-        tick = len(s) - 1
-        while tick > -1:
-            if s[tick] != " ":
-                end = tick + 1
-                while tick > -1 and s[tick] != " ":
-                    tick -= 1
-                newStr += s[tick+1:end]
-                newStr += " "
-            else:
-                tick -= 1
-        s = ""
-        for i in range(len(newStr)-1):
-            s += newStr[i]
-        return s
+        s = s.strip()
+        stack = []
+        retStr = ""
+        i = 0
+        while i < len(s):
+            word = ""
+            while i < len(s) and s[i] != " ":
+                word += s[i]
+                i += 1
+            stack.append(word)
+            i += 1
+        while stack:
+            word = stack.pop(-1)
+            if not word:
+                continue
+            retStr += word + " "
+        return retStr.strip()
