3ee693f509b7e33ff51134b6ede2d9ef71d7b4fe
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-10-28 12:37:26 +0500

    Time: 54 ms (11.88%), Space: 19.4 MB (98.99%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: bool
         """
         nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i-1] == nums[i]:
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
                 return True
         return False
