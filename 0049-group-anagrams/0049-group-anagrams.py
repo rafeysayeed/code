d5565b1d0dbcbfa1b1da0aecf79158e39eac658e
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-11-03 09:38:57 +0500

    Time: 15 ms (92.59%), Space: 16.7 MB (30.62%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,61 +3,15 @@ class Solution(object):
         """
         :type strs: List[str]
         :rtype: List[List[str]]
-        Q. What if there are duplicates
-        Q. What if the element being checked is already in another list (an anagram with two different words) -> In this case, both of them will be in the same list
         """
-        hashM = {}
-        for i in range(len(strs)):
-            ele = str(strs[i])
-            sele = ele
-            sele = ''.join(sorted(sele))
-            print(ele, sele)
-            if sele in hashM.keys():
-                hashM[sele] += [ele]
+        d = {}
+        for i in strs:
+            sw = "".join(sorted(i))
+            if sw in d:
+                d[sw] += [i]
             else:
-                hashM[sele] = [ele]
-        print(hashM)
-        retL = []
-        for v in hashM.values():
-            retL.append(v)
-        return retL
-
-
-
-        # if not len(strs):
-        #     return [[""]]
-        
-        # def MapIt(s):
-        #     hashMap = {}
-        #     for i in s:
-        #         if i not in hashMap:
-        #             hashMap[i] = 1
-        #         else:
-        #             hashMap[i] += 1
-        #     return hashMap
-
-        # listOfHash = []
-        # returnList = []
-        # for i in range(len(strs)):
-        #     hashA = MapIt(strs[i])
-        #     if hashA not in listOfHash:
-        #         listOfHash.append(hashA)
-        #         returnList.append([strs[i]])
-        #     else:
-        #         for j in range(len(listOfHash)):
-        #             if hashA == listOfHash[j]:
-        #                 returnList[j].append(strs[i])
-        # return returnList
-
-        # N^2 Time Limit Exceeded
-        # returnList = []
-        # for i in range(len(strs)):
-        #     if not any(strs[i] in sublist for sublist in returnList):
-        #         newList = [strs[i]]
-        #         hashMapA = MapIt(strs[i])
-        #         for j in range(i+1, len(strs)):
-        #             hashMapB = MapIt(strs[j])
-        #             if hashMapB == hashMapA:
-        #                 newList.append(strs[j])
-        #         returnList.append(newList)
-        # return returnList
+                d[sw] = [i]
+        ll = []
+        for _, i in d.items():
+            ll += [i]
+        return ll
