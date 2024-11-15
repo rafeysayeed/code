ec79c932cb6f5f4ebd9f330c177ade16170865bc
rafeysayeed <57788288+rafeysayeed@users.noreply.github.com>
2024-11-15 10:59:37 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,42 +3,13 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
-        point = 2
-        i = 2
-        [1,0,1,0,1]
-        [0,1,0,3,12]
-        [1,1,0]
         """
-        # # My solution
-        # point = 0
-        # for i in range(len(nums)):
-        #     if nums[i] != 0:
-        #         if point != i:
-        #             nums[point] = nums[i]
-        #             nums[i] = 0
-        #             point += 1
-        #         else:
-        #             point += 1
-        # # Snowball
-        # snowballSize = 0
-        # for i in range(len(nums)):
-        #     if nums[i] == 0:
-        #         snowballSize += 1
-        #     elif snowballSize > 0:
-        #         nums[i - snowballSize] = nums[i]
-        #         nums[i] = 0
-        left = 0
-        right = 0
-        while right < len(nums):
-            while left < len(nums) and nums[left] != 0:
-                left += 1
-            right = left
-            while right < len(nums) and nums[right] == 0:
-                right += 1
-            if right >= len(nums):
-                break
-            temp = nums[left]
-            nums[left] = nums[right]
-            nums[right] = temp
-
-        return nums
+        p1,p2=0,1
+        while p1 < len(nums) and p2 < len(nums):
+            if nums[p1] == 0:
+                while p2 < len(nums) and nums[p2] == 0:
+                    p2 += 1
+                if p2 < len(nums):
+                    nums[p1] = nums[p2]
+                    nums[p2] = 0
+            p1 += 1
